a98a560c2ac5fab56e465c6369bb32f3c579c5a1
Fernando <fspacheco@gmail.com>
2015-04-26 18:34:33 -0300

    versao 2: insere com ponteiro duplo

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
-/*Lista encadeada
-* Fernando S. Pacheco
-* 2015-03-16
-* Baseado no material de Celes e Rangel Cap. 10
+/*!
+* @file main.c
+* @author Fernando S. Pacheco
+* @date 2015-03-16
+* @brief Lista encadeada - Baseado no material de Celes e Rangel Cap. 10
 *
 * Parte 1:
 *  Inicialização, inserção, impressão, liberação
+* Parte 2:
+*  Usando ponteiro para ponteiro (insere2)
 */
 
 #include "listfunc.h"
@@ -17,8 +20,8 @@ int main(void)
    Lista* lst = inicializa();
    printf("##%p##\n",&lst);
    imprime(lst);
-   lst=insere(100, lst);
-   lst=insere(97, lst);
+   insere(100, &lst);
+   insere(97, &lst);
    imprime(lst);
    libera(lst);
    lst=NULL;
